694b3d82ba76b0b3b93c8a75b037869b5118ac15
GivanthaKottapolage <sameeragivantha@gmail.com>
2026-06-28 22:30:22 +0530

    feat(auth): implement google login, otp email verification, and password reset

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,16 @@
-import express from "express";
-
- import { createUser, loginUser } from "../controllers/userController.js";
+import express from "express"
+import { createUser, getAllUsers, getUser, googleLogin, loginUser, sendOTP, updateUserStatus, validateOTPAndUpdatePassword } from "../controllers/userController.js"
 
 const userRouter = express.Router()
 
 userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
+userRouter.get("/", getUser)
+userRouter.post("/google-login", googleLogin)
+userRouter.get("/send-otp/:email", sendOTP)
+userRouter.post("/validate-otp", validateOTPAndUpdatePassword)
+userRouter.get("/all", getAllUsers)
+userRouter.put("/toggle-block/:email", updateUserStatus)
+
 
 export default userRouter
